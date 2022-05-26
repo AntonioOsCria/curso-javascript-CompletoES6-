95b6939fb0d8ba2db1b2940a6dbe5894364bea39
AntonioOsCria <106037579+AntonioOsCria@users.noreply.github.com>
2022-05-26 13:54:12 -0300

    commit

--- Dom para iniciantes/classes-e-atributos/script.js
+++ Dom para iniciantes/classes-e-atributos/script.js
@@ -1,8 +1,14 @@
 // Adicione a classe ativo a todos os itens do menu
-
+const menu = document.querySelector('.menu');
+menu.className += ' ativo';
+console.log(menu);
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
-
+menu.classList.remove('ativo');
 // Verifique se as imagens possuem o atributo alt
+const img = document.querySelector('img');
+const altImg = img.getAttribute('alt');
+console.log(altImg);
+
 
 // Modifique o href do link externo no menu
 
