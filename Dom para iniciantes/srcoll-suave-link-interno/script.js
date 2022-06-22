fd32b54e6dcfcf63c27c4712fa812c3180a21d0a
AntonioOsCria <106037579+AntonioOsCria@users.noreply.github.com>
2022-06-22 17:06:16 -0300

    Nova Aula

--- Dom para iniciantes/srcoll-suave-link-interno/script.js
+++ Dom para iniciantes/srcoll-suave-link-interno/script.js
@@ -38,6 +38,29 @@ function initAccordion() {
   });
  }
 }
-
 initAccordion();
 
+  function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+ function srcollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  const topo = section.offsetTop;
+  
+  window.scrollTo({
+  top: topo,
+  behavior: 'smooth',
+   });
+  };
+linksInternos.forEach((link) => {
+  link.addEventListener('click', srcollToSection); 
+ });
+}
+initScrollSuave();
+
+//Forma alternativa
+  //section.scrollIntoView({
+  //  behavior: 'smooth',
+  //  block: 'start',
+  // });
