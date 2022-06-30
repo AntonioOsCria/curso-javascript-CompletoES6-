6fee46b2d81110592424f214e76644f38df1290b
AntonioOsCria <106037579+AntonioOsCria@users.noreply.github.com>
2022-06-30 17:32:39 -0300

    uup

--- objetos/arrays e loops/script.js
+++ objetos/arrays e loops/script.js
@@ -1,4 +1,13 @@
 // Transforme o objeto abaixo em uma Constructor Function
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+}
+  const antonio = new Pessoa('Antonio', 28);
+
+
+
+
 const pessoa = {
   nome: 'Nome pessoa',
   idade: 0,
@@ -9,6 +18,14 @@ const pessoa = {
 
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
+function individuo(nome, idade) {
+  this.nome = nome;
+  this.iade = idade;
+}
+const joao = new individuo('João', 20);
+const maria = new individuo('Maria', 25);
+const bruno = new individuo('Bruno', 15);
+
 
 
 // Crie uma Constructor Function (Dom) para manipulação
